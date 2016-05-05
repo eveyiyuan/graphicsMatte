3e88cbb031ffa9a37f10986e684c6ebdfb3c1cd1
EVE YUAN <eve.yi.yuan@gmail.com>
2016-05-05 05:38:51 -0700

    added bad simple matting

---
+++ matting.cpp
@@ -0,0 +1,44 @@
+#include <opencv2/opencv.hpp>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace cv;
+
+// Calculates a numerical difference between 2 pixels
+// represented by a vector of 3 color values
+static float colorDiff(const Vec3b &pix1, const Vec3b &pix2) {
+	float diff = 0;
+	for(int i = 0; i < 3; i++) {
+		diff += (pix1[i] - pix2[i]) ^ 2;
+	}
+
+	return sqrt(diff);
+}
+
+void simpleMatte(const Vec3b &bg, const Mat &input, Mat &output, float thresh) {
+	int c = input.cols;
+	int r = input.rows;
+
+	for (int x = 0; x < c; x++) {
+		for (int y = 0; y < r; y++) {
+			const Vec3b color = input.at<Vec3b>(y,x);
+			if(colorDiff(bg, color) < thresh) {
+				output.at<uchar>(y,x) = 0;
+			}
+		}
+	}
+}
+
+int main(int argc, char**argv)
+{
+	float thresh = atof(argv[1]);
+	Mat input = imread("test.png", CV_LOAD_IMAGE_COLOR);
+	Mat output = imread("test.png", CV_LOAD_IMAGE_GRAYSCALE);
+
+	simpleMatte(Vec3b(200,240,0), input, output, thresh);
+	namedWindow("Display", WINDOW_AUTOSIZE);
+	imshow("Display", output);
+	waitKey(0);
+
+	return 0;
+}
