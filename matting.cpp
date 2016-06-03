d237a90a943f4f87cbc8798895ee38759fb3bdb9
EVE YUAN <eve.yi.yuan@gmail.com>
2016-06-03 00:54:08 -0700

    added interactive drawing

--- matting.cpp
+++ matting.cpp
@@ -17,7 +17,14 @@ static float colorDiff(const Vec3b &pix1, const Vec3b &pix2) {
 
 	return sqrt(diff);
 }
+void changeSquare(Mat &input, int x, int y, int l, int val) {
+	for(int r = x - l/2; r < x + l/2; r++) {
+		for (int c = y - l/2; c < y + 1/2; c++) {
+			input.at<uchar>(c, r) = val;
+		}
+	}
 
+}
 void simpleMatte(const Vec3b &bg, const Mat &input, Mat &output, float thresh) {
 	int c = input.cols;
 	int r = input.rows;
@@ -34,31 +41,63 @@ void simpleMatte(const Vec3b &bg, const Mat &input, Mat &output, float thresh) {
 		}
 	}
 }
+
+static bool drawBg = false;
+static bool drawFore = false;
 void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
 	Mat* source = (Mat*) userdata;
-	Vec3b bgColor;
-	Vec3b targetColor;
+
 	if (event == EVENT_LBUTTONDOWN) {
 		cout << "Left button " << x << "," << y << endl;
-		bgColor = (*source).at<Vec3b>(y, x);
+		drawBg = true;
+		// (*source).at<uchar>(y, x) = 0;
+		// changeSquare(*source, x, y, 10, 0);
 	}
 	else if (event == EVENT_RBUTTONDOWN) {
 		cout << "Right button " << x << "," << y << endl;
-		targetColor = (*source).at<Vec3b>(y, x);
+		drawFore = true;
+		// (*source).at<uchar>(y, x) = 255;
+		// changeSquare(*source, x, y, 10, 255);
+	}
+	else if (event == EVENT_MOUSEMOVE) {
+		
+		if (drawBg == true) {
+			cout << "Mouse move " << x << "," << y << endl;
+			(*source).at<uchar>(y, x) = 0;
+			changeSquare(*source, x, y, 10, 0);
+		}
+		else if (drawFore == true) {
+			(*source).at<uchar>(y, x) = 255;
+			changeSquare(*source, x, y, 10, 255);			
+		}
+	}
+	else if (event == EVENT_LBUTTONUP) {
+		drawBg = false;
+	}
+	else if (event == EVENT_RBUTTONUP) {
+		drawFore = false;
 	}
 }
 int main(int argc, char**argv)
 {
 	float thresh = atof(argv[2]);
 	Mat input = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	Mat output = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+	Mat grey = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
 
 
-	simpleMatte(Vec3b(200,240,0), input, output, thresh);
+	
 	namedWindow("Display", WINDOW_AUTOSIZE);
-	setMouseCallback("Display", CallBackFunc, &input);
-	imshow("Display", input);
+	setMouseCallback("Display", CallBackFunc, &grey);
+	while (1) {
+		imshow("Display", grey);
+		if(waitKey(1) != -1) {
+			break;
+		}
+	}
+	// imshow("Display", input);
+	// waitKey(0);
+	simpleMatte(Vec3b(200,240,0), input, grey, thresh);
+	imshow("Display", grey);
 	waitKey(0);
-
 	return 0;
 }
