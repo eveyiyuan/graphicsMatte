73003260a5aecf9762cb9e5d813a0d5e00910a1c
Daniel Gu <dgu@caltech.edu>
2016-06-06 00:59:45 -0700

    UNDEBUGGED geodesics.cpp file added.

---
+++ src/geodesics.cpp
@@ -0,0 +1,114 @@
+// Calculates the geodesic distances between the foreground and background
+// scribbles in color space via a discrtization of our graph and Dijkstra's
+// algorithm. We follow the paper Bai and Sapiro 2008.
+
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+#include <limits>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+// Given an R x C array and an entry index (x, y), returns the index if the
+// array is representedas a 1D row-major array.
+int getIndex(int R, int C, Point p)
+{
+	return p.x * C + p.y;
+}
+
+// Given two adjacent Points s and t, calculates the edge weight
+// W_st = |P_F(s) - P_F(t)|
+// between them. 
+double getWeight(double * P_f, int R, int C, Point s, Point t)
+{
+	s_idx = getIndex(R, C, s);
+	t_idx = getIndex(R, C, t);
+	return abs(P_f[s_idx] - P_f[t_idx]);
+}
+
+// For the 4-stencil discretization suggested in Bai and Sapiro 2008, and using
+// the approximate gradient
+// W_xy = |P_F(x) - P_F(y)|
+// as the weight of edge (x, y), runs Dijkstra's algorithm to find the single-
+// -source shortest paths from the source s.
+vector<double> Dijkstra(double * P_f, int R, int C, Point s)
+{
+	// First, create our priority queue for use in Dijkstra's algorithm.
+	typedef pair<Point, double> QueueElem;
+	// The third coordinate serves as our value; so for convenience we will
+	// write a comparison operator between QueueElems.
+	auto comp[](const QueueElem& q1, const QueueElem& q2) {
+		return q1.second > q2.second;
+	}
+	// Now create our priority queue Q as a vector of QueueElems with
+	// our QueueElem key comparison operator.
+	priority_queue<QueueElem, vector<QueueElem>, decltype(comp)> Q(comp);
+
+	// Now we can start the implementation of Dijkstra's algorithm. First we
+	// maintain an array of distance estimates for each Point. We initialize
+	// each distance to infinity.
+	vector<double> dist (R * C, numeric_limits<double>::max());
+	// Set the distance of the source to 0.
+	dist[getIndex(R, C, s)] = 0.0;
+	// Now populate our priority queue.
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			Point p;
+			p.x = i;
+			p.y = j;
+			Q.push(make_pair(p, dist[getIndex(R, C, p)]));
+		}
+	}
+
+	// Now for the main loop of Dijkstra's algorithm. While the queue is not
+	// empty, we EXTRACT-MIN from the [min-]priority queue and then explore
+	// the [at most] four neighbors p' of the Point p. We then look at the
+	// distance estimate
+	// dist[p] + weight(p, p')
+	// If this distance is smaller than the current distance to p', we update
+	// the distance array. We then push the updated
+	while (!Q.empty())
+	{
+		// Get the Point at the top of the queue.
+		Point u = Q.top().first;
+		// Find the adjacent Points to this Point.
+		int dx[4] = {-1, 0, 1, 0};
+		int dy[4] = {0, -1, 0, 1};
+		for (int i = 0; i < 4; i++)
+		{
+			Point v;
+			v.x = u.x + dx[i];
+			v,y = u.y + dy[i];
+			// Check to see if each neighbor is valid.
+			if (0 <= v.x < R and 0 <= v.y < C)
+			{
+				// Relax with respect to the edge (u, v).
+				double new_dist = dist[getIndex(R, C, u)] + getDistance(P_f, R, C, u, v);
+				if (new_dist < dist[getIndex(R, C, v)])
+				{
+					dist[getIndex(R, C, v)] = new_dist;
+					// The <queue> library doesn't have a DECREASE-KEY
+					// function, so we just add another QueueElem with the same
+					// value but decreased key.
+					Q.push(make_pair(v, new_dist));
+				}
+			}
+		}
+	}
+
+	return dist;
+}
+
+// Actually calculates the geodesic distance from a scribble to a given Point
+// x. This is just the minimum over all points in the scribble 
+double getDistance(double * P_f, int R, int C, vector<Point> scribble, Point x)
+{
+	// Run Dijkstra's algorithm with Point x as the source. For each Point in
+	// the scribble, get the distance.
+	vector<double> dists = Dijkstra(P_f, R, C, x);
+	double min_dist = min(dists); // using the algorithm library
+	return min_dist;
+}
