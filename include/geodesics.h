8be1ecd110c19cb0ace53f530fcd1caf39cdf845
Daniel Gu <dgu@caltech.edu>
2016-06-06 00:58:46 -0700

    UNDEBUGGED geodesics.h added.

---
+++ include/geodesics.h
@@ -0,0 +1,24 @@
+#ifndef _GEODESIC_H_
+#define _GEODESIC_H_
+
+#include "kde.h"
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+// Helper function to transform to 1D indices.
+int getIndex(int R, int C, Point p);
+
+// Gets the edge weight in our discretization.
+double getWeight(double * P_f, int R, int C, Point s, Point t);
+
+// Run's Dijkstra's single-source shortest path algorithm in our 4-stencil
+// discretization of our image from source s.
+vector<double> Dijkstra(double * P_f, int R, int C, Point s);
+
+// Calculates the actual geodesic distance from a given pixel x to a scribble
+// (either the foreground or the background scribble).
+double getDistance(double * P_f, int R, int C, vector<Point> scribble, Point x);
+
+#endif _GEODESIC_H_
